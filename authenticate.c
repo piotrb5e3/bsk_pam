37512500ffcfdb19448a198073a260bb458c2e68
Piotr Bakalarski <piotrb5e3@gmail.com>
2016-11-05 16:00:53 +0100

    Better pam auth failure reporting

--- authenticate.c
+++ authenticate.c
@@ -4,6 +4,8 @@
 #include <security/pam_misc.h>
 #include <security/pam_modules.h>
 
+#include <stdbool.h>
+
 const int AUTH_OK = 1;
 const int AUTH_FAIL = 0;
 
@@ -12,26 +14,32 @@ static struct pam_conv login_conv = {
         NULL
 };
 
+bool pam_status_ok(pam_handle_t *pamh, int pam_status) {
+    if (pam_status != PAM_SUCCESS) {
+        fprintf(stderr, "%s\n\r", pam_strerror(pamh, pam_status));
+        return false;
+    }
+    return true;
+}
+
 int authenticate() {
     int retval = -1;
     pam_handle_t *pamh = NULL;
 
     retval = pam_start("korpusowe", NULL, &login_conv, &pamh);
-    if (pamh == NULL || retval != PAM_SUCCESS)
+    if (pamh == NULL || !pam_status_ok(pamh, retval))
         return AUTH_FAIL;
 
     retval = pam_set_item(pamh, PAM_USER_PROMPT, "Badania korpusowe?\n\r");
 
     retval = pam_authenticate(pamh, 0);
-    if (retval != PAM_SUCCESS) {
+    if (!pam_status_ok(pamh, retval)) {
         retval = pam_end(pamh, retval);
-        if (retval != PAM_SUCCESS)
-            fprintf(stderr, "Pam_end failed!\n\r");
+        pam_status_ok(pamh, retval);
         return AUTH_FAIL;
     } else {
         retval = pam_end(pamh, retval);
-        if (retval != PAM_SUCCESS) {
-            fprintf(stderr, "Pam_end failed!\n\r");
+        if (!pam_status_ok(pamh, retval)) {
             return AUTH_FAIL;
         }
         return AUTH_OK;
